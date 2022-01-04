f78d5904942b40ff7dba52ad28a6dcd4387601bd
vllaim <kamaeft@gmail.com>
2022-01-04 19:14:38 +0300

    Initial commit

---
+++ app/src/main/java/com/kamaeft/lessonsort/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.kamaeft.lessonsort
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kamaeft.lessonsort.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val array = arrayOf(6, 4, 2, 14, 9, 17)
+        bubbleSort(array)
+
+
+    }
+
+    private fun bubbleSort (array: Array<Int>):Array<Int> {
+        var done : Boolean = false
+        while (done == false) {
+            var index: Int = 0
+            done = true
+            while (index <  array.size -1 ) { // Move through the array
+
+                if (array[index] > array[index + 1]) { // If the next item is larger than current
+
+                    // Swap them
+                    val tmp = array[index]
+                    array[index] = array[index + 1]
+                    array[index + 1] = tmp
+
+                    // Mark as not done
+                    done = false
+                }
+
+                //print the array
+                for (element in array) {
+                    element.toString() + 1
+                }
+                    index++
+   
+                binding.tvArrayResult.text = array.contentToString()
+            }
+        }
+        return (array)
+
+    }
+
+
+}
